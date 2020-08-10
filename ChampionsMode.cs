316668ccee2de30683471edd408135695e50556b
kylewill0725 <kylewill0725@outlook.com>
2020-08-10 11:03:13 -0500

    Initial commit

---
+++ ChampionsMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BepInEx;
+
+namespace ChampionsMode
+{
+    [BepInPlugin("com.qwyll.championsmode", "Champions Mode", Gen.VERSION)]
+    public class ChampionsMode : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            
+        }
+    }
+}
---
+++ Gen.cs
@@ -0,0 +1,11 @@
+namespace ChampionsMode
+{
+    public class Gen
+    {
+        public const string MAJOR = "0";
+        public const string MINOR = "0";
+        public const string BUILD = "0";
+
+        public const string VERSION = MAJOR + "." + MINOR + "." + BUILD;
+    }
+}
